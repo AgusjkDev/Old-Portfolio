578de462de779628e2e5578629c98d0cbc78d631
AgusjkDev <agusjkdev7@gmail.com>
2023-01-14 16:34:44 -0300

    Add about section

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Menu } from "./Menu.astro";
 export { default as Socials } from "./Socials.astro";
 export { default as Email } from "./Email.astro";
 export { default as Hero } from "./Hero.astro";
+export { default as About } from "./About.astro";
